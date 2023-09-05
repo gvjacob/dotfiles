74353967c7391a56c2191dcae6eaa01d7e46dafd
gvjacob <gvjacob@outlook.com>
2023-09-05 19:26:17 -0400

    nvim: replace packer with lazy

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -1,106 +1,119 @@
--- Only required if you have packer configured as `opt`
-vim.cmd [[packadd packer.nvim]]
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
-return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
 
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
   --------------------------------------------
   -- Themes
   --------------------------------------------
-  use "EdenEast/nightfox.nvim"
+  "EdenEast/nightfox.nvim",
 
   --------------------------------------------
   -- Git
   --------------------------------------------
-  use 'tpope/vim-fugitive'
-  use 'lewis6991/gitsigns.nvim'
+  "lewis6991/gitsigns.nvim",
 
   --------------------------------------------
   -- Editor
   --------------------------------------------
-  use "lukas-reineke/indent-blankline.nvim"
-  use "windwp/nvim-autopairs"
-  use "github/copilot.vim"
-  use "fedepujol/move.nvim"
-  use "folke/zen-mode.nvim"
-  use "declancm/cinnamon.nvim"
-  use "Olical/vim-enmasse"
-  use "kevinhwang91/nvim-bqf"
-  use "folke/which-key.nvim"
+  "lukas-reineke/indent-blankline.nvim",
+  "windwp/nvim-autopairs",
+  "github/copilot.vim",
+  "fedepujol/move.nvim",
+  "folke/zen-mode.nvim",
+  "declancm/cinnamon.nvim",
+  "Olical/vim-enmasse",
+  "kevinhwang91/nvim-bqf",
+  "folke/which-key.nvim",
 
-  use {
+  {
     'ggandor/leap.nvim',
-    requires = { 'tpope/vim-repeat' }
-  }
+    dependencies = { 'tpope/vim-repeat' }
+  },
 
-  use {
+  {
     "folke/noice.nvim",
-    requires = {
-      "MunifTanjim/nui.nvim",
-    }
-  }
+    dependencies = { "MunifTanjim/nui.nvim" }
+  },
 
-  use {
+  {
     'nvim-lualine/lualine.nvim',
-    requires = {
-      { 'BurntSushi/ripgrep', opt = true },
+    dependencies = {
+      { 'BurntSushi/ripgrep', lazy = true },
     }
-  }
+  },
 
-  use {
+  {
     "nvim-neo-tree/neo-tree.nvim",
     branch = "v2.x",
-    requires = {
+    dependencies = {
       "nvim-lua/plenary.nvim",
       "nvim-tree/nvim-web-devicons",
       "MunifTanjim/nui.nvim",
     }
-  }
+  },
 
-  use { "ellisonleao/glow.nvim", config = function() require("glow").setup() end }
+  {
+    "ellisonleao/glow.nvim",
+    config = function() require("glow").setup() end
+  },
 
   --------------------------------------------
   -- Search
   --------------------------------------------
-  use {
-    'nvim-telescope/telescope.nvim', tag = '0.1.0',
-    requires = {
-      { 'nvim-lua/plenary.nvim' },
-      { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
+  {
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.0',
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
     }
-  }
+  },
 
   --------------------------------------------
   -- LSP
   --------------------------------------------
-  use {
+  {
     "williamboman/mason.nvim",
-    requires = {
+    dependencies = {
       "williamboman/mason.nvim-lspconfig",
       "jay-babu/mason-null-ls.nvim"
     }
-  }
-  use 'neovim/nvim-lspconfig'
-  use 'hrsh7th/nvim-cmp'
-  use 'hrsh7th/cmp-nvim-lsp'
-  use 'hrsh7th/cmp-buffer'
-  use 'hrsh7th/cmp-path'
-  use 'L3MON4D3/LuaSnip'
-  use 'numToStr/Comment.nvim'
-  use 'folke/trouble.nvim'
-  use 'jinzhongjia/LspUI.nvim'
-  use { "nvim-treesitter/nvim-treesitter", { run = ":TSUpdate" } }
+  },
+  "neovim/nvim-lspconfig",
+  "hrsh7th/nvim-cmp",
+  "hrsh7th/cmp-nvim-lsp",
+  "hrsh7th/cmp-buffer",
+  "hrsh7th/cmp-path",
+  "L3MON4D3/LuaSnip",
+  "numToStr/Comment.nvim",
+  "folke/trouble.nvim",
+  "jinzhongjia/LspUI.nvim",
 
-  use {
-    'jose-elias-alvarez/null-ls.nvim',
-    requires = { 'nvim-lua/plenary.nvim' }
-  }
+  {
+    "nvim-treesitter/nvim-treesitter", build = ":TSUpdate"
+  },
+
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" }
+  },
 
   --------------------------------------------
   -- Languages
   --------------------------------------------
-  use 'jose-elias-alvarez/typescript.nvim'
-  use 'NoahTheDuke/vim-just'
-  use 'folke/neodev.nvim'
-end)
+  "jose-elias-alvarez/typescript.nvim",
+  "NoahTheDuke/vim-just",
+  "folke/neodev.nvim",
+})
