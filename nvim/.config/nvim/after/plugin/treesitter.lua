66aa47e91a60f7a339d90caeae912d463af042fd
gvjacob <gvjacob@outlook.com>
2023-09-06 08:35:51 -0400

    nvim: use <leader>b for search buffers

--- nvim/.config/nvim/after/plugin/treesitter.lua
+++ nvim/.config/nvim/after/plugin/treesitter.lua
@@ -1,5 +1,5 @@
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = { "lua", "typescript" },
+  ensure_installed = { "lua", "typescript", "html", "twig", "elixir" },
   highlight = {
     enable = true,
   },
