d061177d0b69a77c80210b7346bdcc75c41b9d18
gvjacob <gvjacob@outlook.com>
2023-11-24 00:04:16 -0500

    Ensure astro is installed for treesitter

--- nvim/.config/nvim/after/plugin/treesitter.lua
+++ nvim/.config/nvim/after/plugin/treesitter.lua
@@ -1,5 +1,5 @@
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = { "lua", "typescript", "html", "twig", "elixir" },
+  ensure_installed = { "lua", "typescript", "html", "twig", "elixir", "astro" },
   highlight = {
     enable = true,
   },
