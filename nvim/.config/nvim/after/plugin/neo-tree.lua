cbc6c2f010486805c32aa93829f8a0aba6256649
gvjacob <gvjacob@outlook.com>
2024-03-14 22:59:14 -0400

    nvim: empty folder icon for neo tree

--- nvim/.config/nvim/after/plugin/neo-tree.lua
+++ nvim/.config/nvim/after/plugin/neo-tree.lua
@@ -4,6 +4,10 @@ require('neo-tree').setup({
   close_if_last_window = true,
   popup_border_style = "rounded",
   default_component_configs = {
+    icon = {
+      folder_empty = "?",
+    },
+
     git_status = {
       symbols = {
         untracked = "|",
