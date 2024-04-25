a788b876f8667bffee3c080146dd7fc9b6ab57b8
gvjacob <gvjacob@outlook.com>
2024-04-25 18:07:04 -0400

    nvim: always hide .DS_Store in oil

--- nvim/.config/nvim/after/plugin/oil.lua
+++ nvim/.config/nvim/after/plugin/oil.lua
@@ -1,1 +1,17 @@
-require("oil").setup()
+require('utils')
+
+require("oil").setup({
+  view_options = {
+    show_hidden = true,
+    is_always_hidden = function(name)
+      if name:match(".DS_Store") then
+        return true
+      end
+
+      return false
+    end
+  }
+})
+
+utils.nmap("<Leader>f", ":Oil<CR>")
+utils.nmap("<Leader>F", ":Oil<CR>")
