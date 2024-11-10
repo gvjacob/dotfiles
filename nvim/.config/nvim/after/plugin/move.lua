55ddfee55a54490534be50de7ec1b01dec3f7e00
gvjacob <gvjacob@outlook.com>
2024-11-10 15:09:44 -0500

    Setup move

--- nvim/.config/nvim/after/plugin/move.lua
+++ nvim/.config/nvim/after/plugin/move.lua
@@ -1,5 +1,7 @@
 local opts = { noremap = true, silent = true }
 
+require('move').setup()
+
 vim.keymap.set('v', 'J', ':MoveBlock(1)<CR>', opts)
 vim.keymap.set('v', 'K', ':MoveBlock(-1)<CR>', opts)
 vim.keymap.set('v', 'H', ':MoveHBlock(-1)<CR>', opts)
