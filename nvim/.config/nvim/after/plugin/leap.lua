efb4ea913a142f1102fa6a883cc090a873b3d1a2
gvjacob <gvjacob@outlook.com>
2024-11-29 19:02:33 -0500

    nvim: disable leap case sensitivity

--- nvim/.config/nvim/after/plugin/leap.lua
+++ nvim/.config/nvim/after/plugin/leap.lua
@@ -1,9 +1,5 @@
 local utils = require('utils')
 
-local leap = require('leap')
-
-leap.opts.case_sensitive = true
-
 utils.map({ 'n', 'v' }, 'f', "<Plug>(leap-forward-to)")
 utils.map({ 'n', 'v' }, 'F', "<Plug>(leap-backward-to)")
 utils.map('n', '<C-w>f', "<Plug>(leap-from-window)")
