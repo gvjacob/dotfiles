28bc9852678f95fd2cec0e9eec9d84de5222ccef
gvjacob <gvjacob@outlook.com>
2026-03-10 14:38:17 -0400

    Fix leap commands

--- nvim/.config/nvim/after/plugin/leap.lua
+++ nvim/.config/nvim/after/plugin/leap.lua
@@ -1,5 +1,5 @@
 local utils = require('utils')
 
-utils.map({ 'n', 'v' }, 'f', "<Plug>(leap-forward-to)")
-utils.map({ 'n', 'v' }, 'F', "<Plug>(leap-backward-to)")
+utils.map({ 'n', 'v' }, 'f', "<Plug>(leap-forward)")
+utils.map({ 'n', 'v' }, 'F', "<Plug>(leap-backward)")
 utils.map('n', '<C-w>f', "<Plug>(leap-from-window)")
